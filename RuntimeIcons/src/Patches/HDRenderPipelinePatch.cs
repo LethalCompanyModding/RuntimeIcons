3569c517cd1e3de5dcd66a5248d54c10a80f4708
Zaggy1024 <Zaggy1024@gmail.com>
2024-11-02 19:44:03 -0500

    Switch the stage camera's color buffer format to include alpha
    Hooking into HDRP for this allows us to drop the custom pass that was causing some modded items to fail to render when they were built without Forward shaders.

---
+++ RuntimeIcons/src/Patches/HDRenderPipelinePatch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace RuntimeIcons.Patches;
+
+[HarmonyPatch(typeof(HDRenderPipeline))]
+internal static class HDRenderPipelinePatch
+{
+    internal static Action<ScriptableRenderContext, Camera> beginCameraRendering;
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(nameof(HDRenderPipeline.Render), [typeof(ScriptableRenderContext), typeof(List<Camera>)])]
+    private static IEnumerable<CodeInstruction> AddCameraRenderHook(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions)
+            .MatchForward(true, [
+                new CodeMatch(insn => insn.IsLdloc()),
+                new CodeMatch(insn => insn.IsLdloc()),
+                new CodeMatch(OpCodes.Callvirt, typeof(List<HDRenderPipeline.RenderRequest>).GetMethod("get_Item", [typeof(int)])),
+                new CodeMatch(insn => insn.opcode == OpCodes.Stloc || insn.opcode == OpCodes.Stloc_S),
+            ]);
+
+        if (matcher.IsInvalid)
+        {
+            RuntimeIcons.Log.LogError($"Failed to hook into HDRP to render icons with transparency.");
+            RuntimeIcons.Log.LogError($"RuntimeIcons will not function correctly.");
+            return instructions;
+        }
+
+        var loadRenderRequest = new CodeInstruction(matcher.Instruction.opcode == OpCodes.Stloc_S ? OpCodes.Ldloc_S : OpCodes.Ldloc, matcher.Instruction.operand);
+        matcher
+            .Advance(1)
+            .Insert([
+                new(loadRenderRequest),
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, typeof(HDRenderPipelinePatch).GetMethod(nameof(BeginCameraRenderingHook), BindingFlags.NonPublic | BindingFlags.Static, null, [typeof(HDRenderPipeline.RenderRequest), typeof(ScriptableRenderContext)], null)),
+            ]);
+
+        return matcher.Instructions();
+    }
+
+    private static void BeginCameraRenderingHook(HDRenderPipeline.RenderRequest request, ScriptableRenderContext context)
+    {
+        beginCameraRendering?.Invoke(context, request.hdCamera.camera);
+    }
+}
